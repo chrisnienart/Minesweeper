fe2d8a6c73f2a32737cd91b66e9b45e8c3dd3f4b
chrisnienart <chris.nienart@minsa.io>
2024-10-19 13:57:09 -0400

    Initialized options screen with values from settings.json

--- options.js
+++ options.js
@@ -1,22 +1,51 @@
-const saveButton = document.getElementById('save');
-
 function fetchInitialSettings() {
     console.log('Fetching initial settings...');
     fetch('settings.json')
         .then(response => response.json())
         .then(settings => {
-            console.log('Initial settings fetched:', settings);
-            const difficultyForm = document.getElementById('difficultyForm');
+            //console.log('Initial settings fetched:', settings);
+            const difficultyMode = document.getElementById('difficultyForm');
             const scoringForm = document.getElementById('scoringForm');
 
-            // Set the initial difficulty
-            const difficultyRadios = difficultyForm.querySelectorAll('input[name="difficulty"]');
-            difficultyRadios.forEach(radio => {
-                if (radio.value === settings.difficulty) {
+            // Set the mode
+            const modeRadios = difficultyMode.querySelectorAll('input[name="mode"]');
+            modeRadios.forEach(radio => {
+                if (radio.value === settings.mode) {
                     radio.checked = true;
-                    console.log('Difficulty radio set:', radio.value);
+                    console.log('Mode radio set:', radio.value);
+                    //this is brittle as the mode and options are implicitly tied
+                    const modeOptions = document.getElementById(settings.mode + 'Options');
+                    modeOptions.style.display = 'block';
                 }
             });
+            
+            // Set the simple options
+            const simpleOptions = document.getElementById('simpleOptions');
+            const simpleRadios = simpleOptions.querySelectorAll('input[name="difficulty"]');
+            simpleRadios.forEach(radio => {
+                if (radio.value === settings.modeOptions.simple.difficulty) {
+                    radio.checked = true;
+                    console.log('Simple difficulty radio set:', radio.value);
+                }
+            });
+
+            // Set the random options
+            const randomMinBoardElement = document.getElementById('randomMinBoardSizeInput');
+            const randomMaxBoardElement = document.getElementById('randomMaxBoardSizeInput');
+            const randomMinPercentElement = document.getElementById('randomMinPercentMinesInput');
+            const randomMaxPercentElement = document.getElementById('randomMaxPercentMinesInput');
+            randomMinBoardElement.value = settings.modeOptions.random.minBoardSize;
+            randomMaxBoardElement.value = settings.modeOptions.random.maxBoardSize;
+            randomMinPercentElement.value = settings.modeOptions.random.minPercentMines;
+            randomMaxPercentElement.value = settings.modeOptions.random.maxPercentMines;
+            console.log('Random difficulty set: ', settings.modeOptions.random);
+
+            // Set the custom options
+            const customBoardElement = document.getElementById('customBoardSizeInput');
+            const customPercentElement = document.getElementById('customPercentMinesInput');
+            customBoardElement.value = settings.modeOptions.custom.boardSize;
+            customPercentElement.value = settings.modeOptions.custom.percentMines;
+            console.log('Custom difficulty set: ', settings.modeOptions.custom);
 
             // Set the initial scoring
             const scoringRadios = scoringForm.querySelectorAll('input[name="scoring"]');
@@ -30,14 +59,14 @@ function fetchInitialSettings() {
         .catch(error => console.error('Error fetching initial settings:', error));
 }
 
-function populateForm(content) {
-    const data = JSON.parse(content);
-    document.getElementById('difficulty').value = data.difficulty;
-    document.getElementById('scoring').value = data.scoring;
-}
+// function populateForm(content) {
+//     const data = JSON.parse(content);
+//     document.getElementById('difficulty').value = data.difficulty;
+//     document.getElementById('scoring').value = data.scoring;
+// }
 
 function saveOptions() {
-    const difficulty = document.querySelector('input[name="difficulty"]:checked')?.value || 'easy';
+    const difficulty = document.querySelector('input[name="mode"]:checked')?.value || 'simple';
     const scoring = document.querySelector('input[name="scoring"]:checked')?.value || 'rated';
 
     // Create a settings object
@@ -63,7 +92,46 @@ function saveOptions() {
     });
 }
 
+document.getElementById('difficultyForm').addEventListener('change', function(event) {
+    const simpleOptions = document.getElementById('simpleOptions');
+    const randomOptions = document.getElementById('randomOptions');
+    const customOptions = document.getElementById('customOptions');
+    const target = event.target;
+
+    if (target.closest('#simpleOptions')) {
+        // If the clicked element is within the menu, keep it open
+        simpleOptions.style.display = 'block';
+    } else {
+        if (event.target.value === 'simple') {
+            simpleOptions.style.display = 'block';
+        } else {
+            simpleOptions.style.display = 'none';
+        }
+    }
+    if (target.closest('#randomOptions')) {
+        // If the clicked element is within the menu, keep it open
+        randomOptions.style.display = 'block';
+    } else {
+        if (event.target.value === 'random') {
+            document.getElementById('randomOptions').style.display = 'block';
+        } else {
+            document.getElementById('randomOptions').style.display = 'none';
+        }
+    }
+    if (target.closest('#customOptions')) {
+        // If the clicked element is within the menu, keep it open
+        customOptions.style.display = 'block';
+    } else {
+        if (event.target.value === 'custom') {
+            document.getElementById('customOptions').style.display = 'block';
+        } else {
+            document.getElementById('customOptions').style.display = 'none';
+        }
+    }
+});
+
 // Save options
+const saveButton = document.getElementById('save');
 saveButton.addEventListener('click', saveOptions);
 
 // Fetch initial settings when the page loads
